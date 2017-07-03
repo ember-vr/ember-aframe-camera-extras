275671405bd66e4cbef90eba9b34b52aee155868
Kelly Selden <kellyselden@gmail.com>
2017-07-03 09:41:05 -0700

    add ember-cli-deploy-git

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/ember-aframe-camera-extras';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
