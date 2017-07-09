510bf12213000d02df865f28543bba424b804b99
Kelly Selden <kellyselden@gmail.com>
2017-07-08 23:17:16 -0700

    add an acceptance test

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
+import './wait-for-aframe';
 
 export default function startApp(attrs) {
   let attributes = Ember.merge({}, config.APP);
