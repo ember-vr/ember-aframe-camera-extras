6c0444e752dc87dd7407a5ffc061ebbe762a47ef
Kelly Selden <kellyselden@gmail.com>
2019-11-02 23:23:39 +0000

    ember-cli 3.13

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [
@@ -15,6 +16,10 @@ module.exports = {
     browser: true
   },
   rules: {
+    'ember/no-jquery': 'error',
+
+    // https://github.com/ember-cli/eslint-plugin-ember/issues/430
+    'ember/no-new-mixins': 'off'
   },
   overrides: [
     // node files
@@ -36,8 +41,7 @@ module.exports = {
         'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: 'script'
       },
       env: {
         browser: false,
