e05c6c8be47300195146b72a4ae24b1b9d593c65
Kelly Selden <kellyselden@gmail.com>
2019-11-02 23:04:45 +0000

    run ember-qunit-codemod

--- tests/integration/components/my-camera-test.js
+++ tests/integration/components/my-camera-test.js
@@ -1,23 +1,20 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('my-camera', 'Integration | Component | my camera', {
-  integration: true
-});
+module('Integration | Component | my camera', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it fires action', function(assert) {
-  let done = assert.async();
-
-  this.set('cameraQueryParams', {
-    _rx: 1,
-    _ry: 2,
-    _px: 3,
-    _py: 4,
-    _pz: 5
+  hooks.beforeEach(function() {
+    this.actions = {};
+    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
   });
 
-  this.on('myAction', params => {
-    assert.deepEqual(params, {
+  test('it fires action', async function(assert) {
+    let done = assert.async();
+
+    this.set('cameraQueryParams', {
       _rx: 1,
       _ry: 2,
       _px: 3,
@@ -25,82 +22,92 @@ test('it fires action', function(assert) {
       _pz: 5
     });
 
-    done();
+    this.actions.myAction = params => {
+      assert.deepEqual(params, {
+        _rx: 1,
+        _ry: 2,
+        _px: 3,
+        _py: 4,
+        _pz: 5
+      });
+
+      done();
+    };
+
+    await render(hbs`
+      <a-scene>
+        {{my-camera
+          cameraQueryParams=cameraQueryParams
+          intervals=(hash
+            myAction=(hash interval=1)
+          )
+          myAction=(action "myAction")
+        }}
+      </a-scene>
+    `);
   });
 
-  this.render(hbs`
-    <a-scene>
-      {{my-camera
-        cameraQueryParams=cameraQueryParams
-        intervals=(hash
-          myAction=(hash interval=1)
-        )
-        myAction=(action "myAction")
-      }}
-    </a-scene>
-  `);
-});
+  test('it adjusts height', async function(assert) {
+    let done = assert.async();
 
-test('it adjusts height', function(assert) {
-  let done = assert.async();
-
-  this.set('cameraQueryParams', {
-    _rx: 1,
-    _ry: 2,
-    _px: 3,
-    _py: 4,
-    _pz: 5
-  });
-
-  this.on('myAction', params => {
-    assert.deepEqual(params, {
+    this.set('cameraQueryParams', {
       _rx: 1,
       _ry: 2,
       _px: 3,
-      _py: 0,
+      _py: 4,
       _pz: 5
     });
 
-    done();
+    this.actions.myAction = params => {
+      assert.deepEqual(params, {
+        _rx: 1,
+        _ry: 2,
+        _px: 3,
+        _py: 0,
+        _pz: 5
+      });
+
+      done();
+    };
+
+    await render(hbs`
+      <a-scene>
+        {{my-camera
+          cameraQueryParams=cameraQueryParams
+          intervals=(hash
+            myAction=(hash interval=1 adjustHeight=true)
+          )
+          myAction=(action "myAction")
+        }}
+      </a-scene>
+    `);
   });
 
-  this.render(hbs`
-    <a-scene>
-      {{my-camera
-        cameraQueryParams=cameraQueryParams
-        intervals=(hash
-          myAction=(hash interval=1 adjustHeight=true)
-        )
-        myAction=(action "myAction")
-      }}
-    </a-scene>
-  `);
-});
+  test('it allows multiple intervals', async function(assert) {
+    let done = assert.async();
 
-test('it allows multiple intervals', function(assert) {
-  let done = assert.async();
+    let count = 0;
+    ['myAction1', 'myAction2'].forEach(action => {
+      this.actions[action] = () => {
+        if (++count === 2) {
+          assert.ok(true);
 
-  let count = 0;
-  ['myAction1', 'myAction2'].forEach(action => {
-    this.on(action, () => {
-      if (++count === 2) {
-        assert.ok(true);
-
-        done();
-      }
+          done();
+        }
+      };
     });
-  });
 
-  this.render(hbs`
-    <a-scene>
-      {{my-camera
-        intervals=(hash
-          myAction1=(hash interval=1)
-          myAction2=(hash interval=1)
-        )
-        myAction1=(action "myAction1")
-        myAction2=(action "myAction2")
-      }}
-    </a-scene>
-  `);
+    await render(hbs`
+      <a-scene>
+        {{my-camera
+          intervals=(hash
+            myAction1=(hash interval=1)
+            myAction2=(hash interval=1)
+          )
+          myAction1=(action "myAction1")
+          myAction2=(action "myAction2")
+        }}
+      </a-scene>
+    `);
+  });
 });
